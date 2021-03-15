9670018649a3eda8a84a14df33190e2767be52d6
Vern Paxson <vern@corelight.com>
2021-03-15 16:30:19 -0700

    handy evaluation scripts

---
+++ src/script_opt/single-bare-test.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+base=/Users/vern/warehouse/zeek-cpp
+test=$base/testing/btest
+so=$base/src/script_opt
+build=$base/build
+gen=CPP-gen-addl.h
+
+export -n ZEEK_USE_CPP
+export ZEEK_HASH_DIR=$test ZEEK_ADD_CPP=
+cd $test
+cp $build/CPP-hashes.dat .
+echo >$gen
+../../auxil/btest/btest $1 >jbuild-$1.out 2>&1
+grep -c '^namespace' $gen
+mv $gen $so
+cd $build
+ninja >& errs || echo build for $1 failed
+
+export -n ZEEK_ADD_CPP
+cd $test
+rm -rf .tmp
+../../auxil/btest/btest -j -a cpp -f jtest.$1.diag $1
